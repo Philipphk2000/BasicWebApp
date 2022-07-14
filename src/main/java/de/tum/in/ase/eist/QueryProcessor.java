b8231fd5fb7b56648285a2d08a0487ee9a14d8f9
Philipp Boettger <philipp.boettger@tum.de>
2022-07-14 11:02:33 +0200

    Name changed

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,10 +13,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Philipp Boettger";
-        } else { // TODO extend the programm here
-
-
-
+        } else {
             return "";
         }
     }
